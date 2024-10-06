4ff70e00204601fb4a8c08c860bc0c3665bb389b
jaanli <jaan.li+github.com@jaan.li>
2024-10-06 11:00:19 -0400

    update

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,7 +17,8 @@ export default {
       name: "Visualizations ",
       pages: [
         {name: "Healthcare data of 1.2M people", path: "/synthetic-healthcare-data"},
-        {name: "850k properties in New York real estate", path: "/new-york-real-estate"}
+        {name: "850k properties in New York real estate", path: "/new-york-real-estate"},
+        {name: "American Community Survey data from 15M+ people", path: "/american-community-survey/"}
       ]
     },
     {
